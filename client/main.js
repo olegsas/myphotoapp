75521ceaa09994a80b9663683776c5271d7d144d
Oleg <o.sas@aisnovations.com>
2017-01-04 11:52:53 +0300

    I added login form. The next step is to customize the navbar

--- client/main.js
+++ client/main.js
@@ -2,5 +2,27 @@ var myApp = angular.module('myApp', ['ngRoute']);
 
 myApp.config(function ($routeProvider) {
   $routeProvider
-    .when('/', {templateUrl: '/partials/home.html'});
+    .when('/', {
+      templateUrl: 'templates/home.html',
+    })
+    .when('/login', {
+      templateUrl: 'templates/login.html',
+      controller: 'loginController'
+    })
+    .when('/logout', {
+      controller: 'logoutController'
+    })
+    .when('/register', {
+      templateUrl: 'templates/register.html',
+      controller: 'registerController'
+    })
+    .when('/one', {
+      template: '<h1>This is page one!</h1>'
+    })
+    .when('/two', {
+      template: '<h1>This is page two!</h1>'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
 });
