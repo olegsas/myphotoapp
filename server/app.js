eceb5f5fa341ac287d7793af18f0360afb6779c3
Oleg <o.sas@aisnovations.com>
2017-01-04 09:34:11 +0300

    Add navbar

--- server/app.js
+++ server/app.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('express-session')({
-    secret: 'myphotoapp key',
+    secret: 'keyboard cat',
     resave: false,
     saveUninitialized: false
 }));
