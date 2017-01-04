176bbdb519b54999e09a823d1c7d5228b5109fb7
Oleg <o.sas@aisnovations.com>
2017-01-04 15:04:01 +0300

    I added restrictions into client/main.js

--- client/main.js
+++ client/main.js
@@ -3,26 +3,43 @@ var myApp = angular.module('myApp', ['ngRoute']);
 myApp.config(function ($routeProvider) {
   $routeProvider
     .when('/', {
-      templateUrl: 'templates/home.html',
+      templateUrl: 'partials/home.html',
+      access: {restricted: true}
     })
     .when('/login', {
-      templateUrl: 'templates/login.html',
-      controller: 'loginController'
+      templateUrl: 'partials/login.html',
+      controller: 'loginController',
+      access: {restricted: false}
     })
     .when('/logout', {
-      controller: 'logoutController'
+      controller: 'logoutController',
+      access: {restricted: true}
     })
     .when('/register', {
-      templateUrl: 'templates/register.html',
-      controller: 'registerController'
+      templateUrl: 'partials/register.html',
+      controller: 'registerController',
+      access: {restricted: false}
     })
     .when('/one', {
-      template: '<h1>This is page one!</h1>'
+      template: '<h1>This is page one!</h1>',
+      access: {restricted: false}
     })
     .when('/two', {
-      template: '<h1>This is page two!</h1>'
+      template: '<h1>This is page two!</h1>',
+      access: {restricted: true}
     })
     .otherwise({
       redirectTo: '/'
     });
+});
+
+
+myApp.run(function ($rootScope, $location, $route, AuthService) {
+  $rootScope.$on('$routeChangeStart',
+  function (event, next, current) {
+    if (next.access.restricted && AuthService.isLoggedIn() === false) {
+      $location.path('/login');
+      $route.reload();
+    }
+  });
 });
